2dc2a82257705b042e8013e78b024293b5cea0e1
tizah <davidzagi93@gmail.com>
2020-02-06 23:20:53 +0100

    added the insert method of a linked list

--- Singly_LinkedList.js
+++ Singly_LinkedList.js
@@ -1,1 +1,24 @@
-console.log("test");
+//the node class
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+//Linked List class
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  insert(data) {
+    this.head = new Node(data, this.head);
+  }
+}
+
+let list = new LinkedList();
+list.insert(5);
+list.insert(6);
+console.log(list);
